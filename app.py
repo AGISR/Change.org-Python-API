69352874ba2334c0a008625de5e6d93fb92cf430
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-11-24 03:46:51 -0800

    Created package for the scraper, and added requests to requirements list

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import changeorgscraper
 
 app = Flask(__name__)
 
 @app.route('/')
 def helloWorld():
 	return "hello world"
+
+sp = changeorgscraper.Scraper()
 
 if __name__ == "__main__":
 	app.run()
